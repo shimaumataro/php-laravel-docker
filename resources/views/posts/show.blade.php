44cb20a69c871cc250a4ab5415d70315a9110fca
testuser <testuser@testusernoMacBook-Air.local>
2023-01-17 00:30:23 +0900

    commentまで

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -19,5 +19,32 @@
             </div>
         </div>
     </div>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <form action="{{ route('comments.store') }}" method="POST">
+            {{csrf_field()}}
+        <input type="hidden" name="post_id" value="{{ $post->id }}">
+                <div class="form-group">
+                    <label>コメント</label>
+                    <textarea class="form-control" 
+                     placeholder="内容" rows="5" name="body"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">コメントする</button>
+            </form>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            @foreach ($post->comments as $comment)
+            <div class="card mt-3">
+                <h5 class="card-header">投稿者：{{ $comment->user->name }}</h5>
+                <div class="card-body">
+                    <h5 class="card-title">投稿日時：{{ $comment->created_at }}</h5>
+                    <p class="card-text">内容：{{ $comment->body }}</p>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
 </div>
 @endsection
